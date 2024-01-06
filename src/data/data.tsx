84c3631248923741b93beca2bfc16e1ec6c7e477
sknooyen <sabrinanguyen21@yahoo.com>
2024-01-05 23:19:02 -0500

    changed background image

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -11,7 +11,7 @@ import {
 import GithubIcon from '../components/Icon/GithubIcon';
 //import InstagramIcon from '../components/Icon/InstagramIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
-import heroImage from '../images/background3.jpg';
+import heroImage from '../images/background1.png';
 //import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
 //import TwitterIcon from '../components/Icon/TwitterIcon';
 import profilepic from '../images/editedheadshot.jpg';

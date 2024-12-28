6ab93eff3081bf38aa36592c1c37a72a97058a26
sknooyen <sabrinanguyen21@yahoo.com>
2024-12-28 17:54:06 -0500

    testing changse to favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,18 @@ import 'tailwindcss/tailwind.css';
 import '../globalStyles.scss';
 
 import type {AppProps} from 'next/app';
+import Head from 'next/head';
 import {memo} from 'react';
 
 const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
   return (
     <>
+      <Head>
+        <link href="/favicon.ico" rel="icon" />
+        <link href="/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+        <link href="/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+        <link href="/favicon.png" rel="apple-touch-icon" />
+      </Head>
       <Component {...pageProps} />
     </>
   );

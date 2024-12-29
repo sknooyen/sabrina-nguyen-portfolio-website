7ada8ccd7fd39e1c9e53a13e048fe2808f197b8f
sknooyen <sabrinanguyen21@yahoo.com>
2024-12-28 19:30:49 -0500

    testing dark/light favicons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import 'tailwindcss/tailwind.css';
+/*import 'tailwindcss/tailwind.css';
 import '../globalStyles.scss';
 
 import type {AppProps} from 'next/app';
@@ -15,5 +15,55 @@ const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
     </>
   );
 });
+
+export default MyApp;
+*/
+
+import 'tailwindcss/tailwind.css';
+import '../globalStyles.scss';
+
+import type {AppProps} from 'next/app';
+import Head from 'next/head';
+import {useEffect} from 'react';
+
+// eslint-disable-next-line react-memo/require-memo
+const MyApp = ({Component, pageProps}: AppProps): JSX.Element => {
+  useEffect(() => {
+    const setFavicon = (theme: string) => {
+      const favicon = document.querySelector("link[rel='icon']");
+      if (favicon) {
+        favicon.setAttribute(
+          "href",
+          theme === "dark" ? "/favicon-white.png" : "/favicon-dark-blue.png"
+        );
+      }
+    };
+
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    setFavicon(mediaQuery.matches ? "dark" : "light");
+
+    const listener = (e: MediaQueryListEvent) => {
+      setFavicon(e.matches ? "dark" : "light");
+    };
+
+    mediaQuery.addEventListener("change", listener);
+
+    return () => mediaQuery.removeEventListener("change", listener);
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <link
+          href="/favicon-white.png"
+          rel="icon"
+          sizes="192x192"
+          type="image/png"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
 
 export default MyApp;

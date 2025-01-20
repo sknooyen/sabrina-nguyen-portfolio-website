a1c28c3256c3a54f42f6f103767f45d697e54f7c
sknooyen <sabrinanguyen21@yahoo.com>
2025-01-19 20:38:16 -0500

    deleted more comments, updated resume link

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -10,15 +10,10 @@ import profilePic from '../images/editedheadshot.jpg';
 import planet_parv from '../images/portfolio/Planet Parv.jpg';
 import {
   About,
-  //ContactSection,
-  //ContactType,
   Hero,
   HomepageMeta,
   PortfolioItem,
-  //SkillGroup,
   Social,
-  //TestimonialSection,
-  //TimelineItem,
 } from './dataDef';
 
 /**
@@ -67,7 +62,7 @@ export const heroData: Hero = {
   ),
   actions: [
     {
-      href: 'Sabrina Nguyen Resume.pdf',
+      href: '/Sabrina Nguyen Resume.pdf',
       text: 'Resume',
       primary: true,
       Icon: ArrowDownTrayIcon,
@@ -238,62 +233,6 @@ export const experience: TimelineItem[] = [
     ),
   },
 ];
-*/
-
-/* Testimonial section
- 
-export const testimonial: TestimonialSection = {
-  imageSrc: testimonialImage,
-  testimonials: [
-    {
-      name: 'John Doe',
-      text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
-    },
-    {
-      name: 'Jane Doe',
-      text: 'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
-    },
-    {
-      name: 'Someone else',
-      text: 'Add several of these, and keep them as fresh as possible, but be sure to focus on quality testimonials with strong highlights of your skills/work ethic.',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/69.jpg',
-    },
-  ],
-};
-*/
-
-/**
- * Contact section
- 
-
-export const contact: ContactSection = {
-  headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
-  items: [
-    {
-      type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
-    },
-    {
-      type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
-    },
-    {
-      type: ContactType.Instagram,
-      text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
-    },
-    {
-      type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
-    },
-  ],
-};
 */
 
 /**
